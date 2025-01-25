668221bb9696f8044f5727482b89f0251d18a255
syamgith <syamgithksudheesh@gmail.com>
2025-01-25 23:49:32 +0530

    debug 2 dreams adding in 1st entry

--- dream_journal_app/lib/features/dreams/presentation/pages/dreams_page.dart
+++ dream_journal_app/lib/features/dreams/presentation/pages/dreams_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/constants/app_colors.dart';
-import '../../../../core/constants/app_text_styles.dart';
 import '../../providers/dreams_provider.dart';
 import '../widgets/dream_card.dart';
 //import '../widgets/dream_icon.dart';
